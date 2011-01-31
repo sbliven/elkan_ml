9ea118c9a323ce68c293aad4b83c098fcc7fcd33
Spencer Bliven <sbliven@ucsd.edu>
2011-01-31 13:28:27 -0800

    Allow calculation of the gradient of more than one point at a time

--- SGD/logRegGradient.m
+++ SGD/logRegGradient.m
@@ -1,16 +1,19 @@
-function gradient = logRegGradient(beta, trainingRow)
+function gradient = logRegGradient(training, beta)
 % Calculates the gradient of the logistic function given parameters beta
 %
 % Args:
 %   beta: A Dx1 vector of parameters
-%   trainingRow: A row of training data, as follows
-%       trainingRow(1)      label (0 or 1)
-%       trainingRow(2:D)    D-1 dimensional training point
+%   training: A NxD matrix of training data, as follows
+%       column 1      label (0 or 1)
+%       columns 2:D   D-1 dimensional training points
 %
-y = trainingRow(1);
-x = trainingRow(2:end);
+% Output:
+%   gradient: A NxD matrix, giving the gradient w.r.t. beta at each point
+[N,D] = size(training);
+
+y = training(:,1);
+x = training(:,2:end);
 
 p = logReg(x,beta);
-
-gradient = (y-p)*[1 x]';
+gradient = repmat(y-p,1,D) .* [ones(N,1) x];
 
