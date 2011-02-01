84bbeba0e5f1d98cd55d966ea4e33dc98223be18
Spencer Bliven <sbliven@ucsd.edu>
2011-01-31 21:27:35 -0800

    Adding exponential.m, another function to test checkgrad.m

---
+++ SGD/exponential.m
@@ -0,0 +1,11 @@
+function [y,dy] = exponential(b,x)
+% calculates the function
+%   yi = exp(-b'*x)
+% and gradient
+%   dyi/dbj = -xij*exp(-b'*x)
+
+y = exp(-x*b);
+
+if nargout > 1
+    dy = repmat(y,1,size(x,2)) .* x;
+end
