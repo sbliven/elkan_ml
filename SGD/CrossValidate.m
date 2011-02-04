644415855c87361418d1cd5b1190702d06100cae
Landstad <devnull@localhost>
2011-02-04 14:24:13 -0800

    Added CrossValidateLCL and edited CrossValidate
    Added CrossValidateLCL and edited CrossValidate such that we
    will have a working prediction CV if we need it too

--- SGD/CrossValidate.m
+++ SGD/CrossValidate.m
@@ -1,57 +1,45 @@
-function [tp,fp,tn,fn,info]=CrossValidate(trainingData)
+function [tn,tp,fn,fp]=CrossValidate(trainingData,betas0,lambda)
 %args:
 %outputs:
-%tp,fp,tn,fn used as usual for CV.
-%info is used if one uses a non-classification method (eg SE)
+%true posives, false positives, true negatives, false negatives
 %
 %input
 %trainingData - NxD matrix where each of the N lines is a row and every 
 %first column of the rows represent a label and each of the D-1 remaining
 %columns are dimensions.
+%betas0 - the betas to start with
+%lambda - the learning rate
 
 %k is number of folds
 k=5;
 %Obtaining indices for
 indices = crossvalind('Kfold', length(trainingData),k);
 
-%true posives, false positives, true negatives, false negatives
 tp=zeros(4,1);
 fp=zeros(4,1);
 tn=zeros(4,1);
 fn=zeros(4,1);
 
-%obtaining the trainingData sizes
-[N,M]=size(trainingData);
-
 %For each of the k folds, use this as test set and train the model on
 %the remaining k-1 sets
+
 for k=1:5
-trainingFold=zeros(N*4/5,M);
-testFold=zeros(N/5,M);
-    
     %separate into training and test set
-    for i=1:length(indices)
-        if(indices(i)==k)
-            testFold(i,:)=trainingData(i,:);
-        else
-            trainingFold(i,:)=trainingData(i,:);
-        end
-    end
+    testFold = trainingData(indices==k,:);
+    trainingFold = trainingData(indices~=k,:);
     
-    %TODO 1: train model
-    %betas=SGD(trainingFold,something something);
-    
+    %Train model
+    betas=SGD(trainingFold,betas0,@logisticLCL,lambda);
     
     %run tests
     tp(k)=0;
     tn(k)=0;
     fp(k)=0;
     fn(k)=0;
-    for i=0:length(trainingData)
-        %TODO 2: predict
-        %regular CV:
-        label=predict(trainingData(i,2:end),betas);
-        if (label == trainingData(i,1))
+    for i=0:length(testFold)
+        %TODO 2: no prediction function defined.
+        label=predict(testFold(i,2:end),betas);
+        if (label == testFold(i,1))
             if(label==true) %or spend, whatever
                 tp(k)=tp(k)+1;
             else
@@ -64,10 +52,7 @@ testFold=zeros(N/5,M);
                 fp(k)=fp(k)+1;
             end
         end
-        %TODO 3:
-        %CV that just returns a number (or several?) containing information
-        %about how well it did: (eg SE)
-        %info(k)=getInfo(testFold,betas);
     end
+
 end
 end
---
+++ SGD/CrossValidateLCL.m
@@ -0,0 +1,31 @@
+function [lcl]=CrossValidateLCL(trainingData,betas0,lambda)
+%args:
+%outputs:
+%lcl, a 2 by k matrix with the lcl's and gradients for each of the k
+%testFolds
+%
+%input
+%trainingData - NxD matrix where each of the N lines is a row and every 
+%first column of the rows represent a label and each of the D-1 remaining
+%columns are dimensions.
+
+%k is number of folds
+k0=5;
+%Obtaining indices for training and test-fold
+indices = crossvalind('Kfold', length(trainingData),k);
+
+%For each of the k folds, use this as test set and train the model on
+%the remaining k-1 sets
+lcl=zeros(2,k);
+for k=1:k0
+    %separate into training and test set
+    testFold = trainingData(indices==k,:);
+    trainingFold = trainingData(indices~=k,:);
+    
+    %TODO 1: train model
+    betas=SGD(trainingFold,betas0,@logisticLCL,lambda);
+    
+    %Calculate lcl for the
+    lcl(k)=sum(logisticLCL(betas,testFold));
+end
+end
