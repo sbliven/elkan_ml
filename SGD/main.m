eefcba3a3c7382f9dfc627729cad893fce885eb0
Spencer Bliven <sbliven@ucsd.edu>
2011-02-04 12:42:35 -0800

    Split up gradient test code from main.m

--- SGD/main.m
+++ SGD/main.m
@@ -39,38 +39,4 @@ YXSpendWomens = [womens(:,29) womens(:,1:26) womens(:,28)];
 YXSpendMens = [mens(:,29) mens(:,1:26) mens(:,28)];
 YXSpendNoemail = [noemail(:,29) noemail(:,1:26) noemail(:,28)];
 
-
-
-
-%% Checking accuracy of functions
-% The following should produce numbers near 0 for all beta
-
-N = 1000;
-D = 2;
-y=zeros(N,1);
-y(1:N/2) = 1;
-x = randn(N,2);
-% X|Y=0 ~ MVN( [-1;0], [1;4])
-% X|Y=1 ~ MVN( [1;3],[1;4])
-x(:,1) = x(:,2) + 2*y -1;
-x(:,2) = 2*x(:,2) + 3*y;
-z = [y x];
-z = z(randperm(N),:);
-
-b = zeros(D+1,1);
-
-% Can also check logistic, exponential, or linear
-checkgrad(@logisticLCL, b, 1e-6, z)
-
-%% Run gradient descent
-lambda=0.0001;
-numEpochs = 1000;
-bs = zeros(numEpochs,D+1);
-lcl = zeros(numEpochs,1);
-for epoch = 1:numEpochs
-    b = GD(z,b,@logisticLCL, lambda);
-    bs(epoch,:) = b';
-    lcl(epoch) = sum(logisticLCL(b,z));
-end
-plot(lcl)
-
+clear womens mens noemail;
