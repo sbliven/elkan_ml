66149ac98d4188b318a5fe82deadd7c4699b39c2
jhoelzler <devnull@localhost>
2011-02-04 10:30:02 -0800

    Intercept added Data normalized Report slighly edited

--- SGD/main.m
+++ SGD/main.m
@@ -11,27 +11,33 @@ womens = csvread('./Dataset/WomensEmail.csv');
 mens = csvread('./Dataset/MensEmail.csv');
 noemail = csvread('./Dataset/NoEmail.csv');
 
+%%normailze data with mean 0 and standard deviation 1 using mapstd
+womens = mapstd(womens')';
+mens = mapstd(mens')';
+noemail = mapstd(noemail')';
+
+
 
 %% The YX-Vector for linear regression 1 (visit)
 %womens/mens/noemail
 %y visit vector | x vector
-YXVisitWomens = [womens(:,26) womens(:,1:25)];
-YXVisitMens = [mens(:,26) mens(:,1:25)];
-YXVisitNoemail = [noemail(:,26) noemail(:,1:25)];
+YXVisitWomens = [womens(:,27) womens(:,1:26)];
+YXVisitMens = [mens(:,27) mens(:,1:26)];
+YXVisitNoemail = [noemail(:,27) noemail(:,1:26)];
 
 %% The YX-Vectors for linear regression 2 (purchase)
 %womens/mens/noemail
 %y purchase | x vector and visit entry
-YXPurchaseWomens = [womens(:,26) womens(:,1:26)];
-YXPurchaseMens = [mens(:,26) mens(:,1:26)];
-YXPurchaseNoemail = [noemail(:,26) noemail(:,1:26)];
+YXPurchaseWomens = [womens(:,28) womens(:,1:27)];
+YXPurchaseMens = [mens(:,28) mens(:,1:27)];
+YXPurchaseNoemail = [noemail(:,28) noemail(:,1:27)];
 
 %% The YX-Vectors for nlinfit
 %womens/mens/noemail
 %y spend | x vector and purchase entry
-YXSpendWomens = [womens(:,28) womens(:,1:25) womens(:,27)];
-YXSpendMens = [mens(:,28) mens(:,1:25) mens(:,27)];
-YXSpendNoemail = [noemail(:,28) noemail(:,1:25) noemail(:,27)];
+YXSpendWomens = [womens(:,29) womens(:,1:26) womens(:,28)];
+YXSpendMens = [mens(:,29) mens(:,1:26) mens(:,28)];
+YXSpendNoemail = [noemail(:,29) noemail(:,1:26) noemail(:,28)];
 
 
 
