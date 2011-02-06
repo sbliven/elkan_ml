dee68a3e9d91d4e2b924ad0618133ab859226749
Spencer Bliven <spencer.bliven@gmail.com>
2011-02-06 01:40:52 -0800

    Added optional param to change lambda each epoch. Improved comments

--- SGD/logisticRegression.m
+++ SGD/logisticRegression.m
@@ -1,22 +1,46 @@
-function [b, bs, lcl] = logisticRegression(yx, numEpochs, lambda0, beta0 )
+function [b, bs, lcl] = logisticRegression(yx, numEpochs, lambda0, beta0, learnRate, gdAlg )
 % Runs logistic regression
+%
+% Args (1 required):
+%   yx          The training data, with the first column specifying the label for each point
+%   numEpochs   Number of epochs to perform [default=1]
+%   lambda0     Initial value of lambda [default=0.1]
+%   beta0       Initial value of beta [default=zero]
+%   learnRate   Rate of change in lambda for each epoch. [default=0]
+%               lambda = 1/(1/lambda0 + learnRate*epoch) 
+%   gdAlg       Function handle for gradient algorithm. [default=@SGD]
 
 [N,D] = size(yx);
 
-if nargin < 4
+% Set default parameters
+if nargin < 2 || isempty(numEpochs)
+    numEpochs = 1;
+end
+
+if nargin < 3 || isempty(lambda0)
+    lambda0=0.1;
+end
+lambda = lambda0;
+
+
+if nargin < 4 || isempty(beta0)
     b = zeros(D,1);
 else
     b = beta0;
 end
 
-if nargin < 3
-    lambda0=0.1;
+if nargin < 5 || isempty(learnRate)
+    learnRate = 0;
 end
-lambda = lambda0;
 
-if nargin < 2
-    numEpochs = 1000;
+if nargin < 6 || isempty(gdAlg)
+    gdAlg = @SGD;
 end
+
+
+
+
+% calculate epochs
 
 if nargout > 1
     bs = zeros(numEpochs,D);
@@ -24,11 +48,13 @@ if nargout > 1
 end
 
 for epoch = 1:numEpochs
-    b = SGD(yx,b,@logisticLCL, lambda);
+    b = gdAlg(yx,b,@logisticLCL, lambda);
     
     if nargout > 1
         bs(epoch,:) = b';
         lcl(epoch) = sum(logisticLCL(b,yx));
     end
+    
+    lambda = 1/(1/lambda0 + learnRate*epoch);
 end
 
