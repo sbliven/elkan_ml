c27d4426c5abada893c8dfaf3e488be5bbc1e66b
Spencer Bliven <spencer.bliven@gmail.com>
2011-02-06 01:42:17 -0800

    Updated main to calculate beta vectors

--- SGD/main.m
+++ SGD/main.m
@@ -47,5 +47,40 @@ YXSpendNoemail = [noemail(:,28) noemail(:,1:25) noemail(:,27)];
 
 clear womens mens noemail;
 
-yx = balanceSamples(YXVisitWomens);
-[b, bs, lcl] = logisticRegression(yx, 1, 0.1,ones(27,1));
+beta0 = zeros(size(yx,2),1);
+
+YXVisitWomensBal = balanceSamples(YXVisitWomens);
+[b, VisitWomensBetas, VisitWomensLCLs] = logisticRegression(YXVisitWomensBal, 100, 0.1, beta0, 1 );
+
+YXVisitMensBal = balanceSamples(YXVisitMens);
+[b, VisitMensBetas, VisitMensLCLs] = logisticRegression(YXVisitMensBal, 100, 0.1, beta0, 1 );
+
+YXVisitNoemailBal = balanceSamples(YXVisitNoemail);
+[b, VisitNoemailBetas, VisitNoemailLCLs] = logisticRegression(YXVisitNoemailBal, 100, 0.1, beta0, 1 );
+
+YXPurchaseWomensBal = balanceSamples(YXPurchaseWomens);
+[b, PurchaseWomensBetas, PurchaseWomensLCLs] = logisticRegression(YXPurchaseWomensBal, 100, 0.1, beta0, 1 );
+
+YXPurchaseMensBal = balanceSamples(YXPurchaseMens);
+[b, PurchaseMensBetas, PurchaseMensLCLs] = logisticRegression(YXPurchaseMensBal, 100, 0.1, beta0, 1 );
+
+YXPurchaseNoemailBal = balanceSamples(YXPurchaseNoemail);
+[b, PurchaseNoemailBetas, PurchaseNoemailLCLs] = logisticRegression(YXPurchaseNoemailBal, 100, 0.1, beta0, 1 );
+
+figure; plot(VisitWomensLCLs,'-s'); title("VisitWomensLCLs"); xlab("Epoch"); ylab("LCL");
+figure; plot(VisitWomensBetas(:,1),'-x',betas(:,2:end),'-s'); title("VisitWomensBetas"); xlab("Epoch");
+
+figure; plot(VisitMensLCLs,'-s'); title("VisitMensLCLs"); xlab("Epoch"); ylab("LCL");
+figure; plot(VisitMensBetas(:,1),'-x',betas(:,2:end),'-s'); title("VisitMensBetas"); xlab("Epoch");
+
+figure; plot(VisitNoemailLCLs,'-s'); title("VisitNoemailLCLs"); xlab("Epoch"); ylab("LCL");
+figure; plot(VisitNoemailBetas(:,1),'-x',betas(:,2:end),'-s'); title("VisitNoemailBetas"); xlab("Epoch");
+
+figure; plot(PurchaseWomensLCLs,'-s'); title("PurchaseWomensLCLs"); xlab("Epoch"); ylab("LCL");
+figure; plot(PurchaseWomensBetas(:,1),'-x',betas(:,2:end),'-s'); title("PurchaseWomensBetas"); xlab("Epoch");
+
+figure; plot(PurchaseMensLCLs,'-s'); title("PurchaseMensLCLs"); xlab("Epoch"); ylab("LCL");
+figure; plot(PurchaseMensBetas(:,1),'-x',betas(:,2:end),'-s'); title("PurchaseMensBetas"); xlab("Epoch");
+
+figure; plot(PurchaseNoemailLCLs,'-s'); title("PurchaseNoemailLCLs"); xlab("Epoch"); ylab("LCL");
+figure; plot(PurchaseNoemailBetas(:,1),'-x',betas(:,2:end),'-s'); title("PurchaseNoemailBetas"); xlab("Epoch");
