1072a91461043d55db6dc6c9f4e6dcea0e765553
Spencer Bliven <sbliven@ucsd.edu>
2011-02-07 14:39:35 -0800

    Interleaved figure generation with logistic regression as a status report

--- SGD/main.m
+++ SGD/main.m
@@ -53,50 +53,50 @@ beta0 = zeros(size(YXVisitWomens,2),1);
 lambda = 0.1;
 epochs = 250;
 
+set (gcf, "paperposition", [0.5 0.5 4 3]); % eps size
+
 YXVisitWomensBal = balanceSamples(YXVisitWomens);
 [b, VisitWomensBetas, VisitWomensLCLs] = logisticRegression(YXVisitWomensBal, epochs, lambda, beta0, 1, [], alpha );
-
-YXVisitMensBal = balanceSamples(YXVisitMens);
-[b, VisitMensBetas, VisitMensLCLs] = logisticRegression(YXVisitMensBal, epochs, lambda, beta0, 1, [], alpha );
-
-YXVisitNoemailBal = balanceSamples(YXVisitNoemail);
-[b, VisitNoemailBetas, VisitNoemailLCLs] = logisticRegression(YXVisitNoemailBal, epochs, lambda, beta0, 1, [], alpha );
-
-YXPurchaseWomensBal = balanceSamples(YXPurchaseWomens);
-[b, PurchaseWomensBetas, PurchaseWomensLCLs] = logisticRegression(YXPurchaseWomensBal, epochs, lambda, beta0, 1, [], alpha );
-
-YXPurchaseMensBal = balanceSamples(YXPurchaseMens);
-[b, PurchaseMensBetas, PurchaseMensLCLs] = logisticRegression(YXPurchaseMensBal, epochs, lambda, beta0, 1, [], alpha );
-
-YXPurchaseNoemailBal = balanceSamples(YXPurchaseNoemail);
-[b, PurchaseNoemailBetas, PurchaseNoemailLCLs] = logisticRegression(YXPurchaseNoemailBal, epochs, lambda, beta0, 1, [], alpha );
-
-set (gcf, "paperposition", [0.5 0.5 4 3]); % eps size
 
 figure (10); plot(VisitWomensLCLs,'-s'); title("VisitWomensLCLs"); xlabel("Epoch"); ylabel("LCL");
 print('-deps','report/VisitWomensLCLs.eps');
 figure (11); plot(VisitWomensBetas(:,1),'-x',VisitWomensBetas(:,2:end),'-s'); title("VisitWomensBetas"); xlabel("Epoch");
 print('-deps','report/VisitWomensBetas.eps');
 
+YXVisitMensBal = balanceSamples(YXVisitMens);
+[b, VisitMensBetas, VisitMensLCLs] = logisticRegression(YXVisitMensBal, epochs, lambda, beta0, 1, [], alpha );
+
 figure (12); plot(VisitMensLCLs,'-s'); title("VisitMensLCLs"); xlabel("Epoch"); ylabel("LCL");
 print('-deps','report/VisitMensLCLs.eps');
 figure (13); plot(VisitMensBetas(:,1),'-x',VisitMensBetas(:,2:end),'-s'); title("VisitMensBetas"); xlabel("Epoch");
 print('-deps','report/VisitMensBetas.eps');
+
+YXVisitNoemailBal = balanceSamples(YXVisitNoemail);
+[b, VisitNoemailBetas, VisitNoemailLCLs] = logisticRegression(YXVisitNoemailBal, epochs, lambda, beta0, 1, [], alpha );
 
 figure (14); plot(VisitNoemailLCLs,'-s'); title("VisitNoemailLCLs"); xlabel("Epoch"); ylabel("LCL");
 print('-deps','report/VisitNoemailLCLs.eps');
 figure (15); plot(VisitNoemailBetas(:,1),'-x',VisitNoemailBetas(:,2:end),'-s'); title("VisitNoemailBetas"); xlabel("Epoch");
 print('-deps','report/VisitNoemailBetas.eps');
 
+YXPurchaseWomensBal = balanceSamples(YXPurchaseWomens);
+[b, PurchaseWomensBetas, PurchaseWomensLCLs] = logisticRegression(YXPurchaseWomensBal, epochs, lambda, beta0, 1, [], alpha );
+
 figure (16); plot(PurchaseWomensLCLs,'-s'); title("PurchaseWomensLCLs"); xlabel("Epoch"); ylabel("LCL");
 print('-deps','report/PurchaseWomensLCLs.eps');
 figure (17); plot(PurchaseWomensBetas(:,1),'-x',PurchaseWomensBetas(:,2:end),'-s'); title("PurchaseWomensBetas"); xlabel("Epoch");
 print('-deps','report/PurchaseWomensBetas.eps');
 
+YXPurchaseMensBal = balanceSamples(YXPurchaseMens);
+[b, PurchaseMensBetas, PurchaseMensLCLs] = logisticRegression(YXPurchaseMensBal, epochs, lambda, beta0, 1, [], alpha );
+
 figure (18); plot(PurchaseMensLCLs,'-s'); title("PurchaseMensLCLs"); xlabel("Epoch"); ylabel("LCL");
 print('-deps','report/PurchaseMensLCLs.eps');
 figure (19); plot(PurchaseMensBetas(:,1),'-x',PurchaseMensBetas(:,2:end),'-s'); title("PurchaseMensBetas"); xlabel("Epoch");
 print('-deps','report/PurchaseMensBetas.eps');
+
+YXPurchaseNoemailBal = balanceSamples(YXPurchaseNoemail);
+[b, PurchaseNoemailBetas, PurchaseNoemailLCLs] = logisticRegression(YXPurchaseNoemailBal, epochs, lambda, beta0, 1, [], alpha );
 
 figure (20); plot(PurchaseNoemailLCLs,'-s'); title("PurchaseNoemailLCLs"); xlabel("Epoch"); ylabel("LCL");
 print('-deps','report/PurchaseNoemailLCLs.eps');
