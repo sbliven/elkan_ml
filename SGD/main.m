8b8ffce6ef1d18550c532fa5e0ae19b99236bdd1
Spencer Bliven <quantum7@gmail.com>
2011-02-07 23:31:24 -0800

    made more matlab-friendly with quotes

--- SGD/main.m
+++ SGD/main.m
@@ -7,7 +7,7 @@
 
 %% Read in files from new created csv-files
 % Desription of file format @ report
-if ~exist('womens') || ~exist('mens') || ~exist('noemail')
+if ~exist('womens','var') || ~exist('mens','var') || ~exist('noemail','var')
     womens = csvread('./Dataset/WomensEmail.csv');
     mens = csvread('./Dataset/MensEmail.csv');
     noemail = csvread('./Dataset/NoEmail.csv');
@@ -53,54 +53,54 @@ beta0 = zeros(size(YXVisitWomens,2),1);
 lambda = 0.1;
 epochs = 250;
 
-set (gcf, "paperposition", [0.5 0.5 4 3]); % eps size
+set (gcf, 'paperposition', [0.5 0.5 4 3]); % eps size
 
 YXVisitWomensBal = balanceSamples(YXVisitWomens);
 [b, VisitWomensBetas, VisitWomensLCLs] = logisticRegression(YXVisitWomensBal, epochs, lambda, beta0, 1, [], alpha );
 
-figure (10); plot(VisitWomensLCLs,'-s'); title("VisitWomensLCLs"); xlabel("Epoch"); ylabel("LCL");
+figure (10); plot(VisitWomensLCLs,'-s'); title('VisitWomensLCLs'); xlabel('Epoch'); ylabel('LCL');
 print('-depsc2','report/VisitWomensLCLs.eps');
-figure (11); plot(VisitWomensBetas(:,1),'-','linewidth',5,VisitWomensBetas(:,2:end),'-'); title("VisitWomensBetas"); xlabel("Epoch");
+figure (11); plot(VisitWomensBetas(:,1),'-','linewidth',5,VisitWomensBetas(:,2:end),'-'); title('VisitWomensBetas'); xlabel('Epoch');
 print('-depsc2','report/VisitWomensBetas.eps');
 
 YXVisitMensBal = balanceSamples(YXVisitMens);
 [b, VisitMensBetas, VisitMensLCLs] = logisticRegression(YXVisitMensBal, epochs, lambda, beta0, 1, [], alpha );
 
-figure (12); plot(VisitMensLCLs,'-s'); title("VisitMensLCLs"); xlabel("Epoch"); ylabel("LCL");
+figure (12); plot(VisitMensLCLs,'-s'); title('VisitMensLCLs'); xlabel('Epoch'); ylabel('LCL');
 print('-depsc2','report/VisitMensLCLs.eps');
-figure (13); plot(VisitMensBetas(:,1),'-','linewidth',5,VisitMensBetas(:,2:end),'-'); title("VisitMensBetas"); xlabel("Epoch");
+figure (13); plot(VisitMensBetas(:,1),'-','linewidth',5,VisitMensBetas(:,2:end),'-'); title('VisitMensBetas'); xlabel('Epoch');
 print('-depsc2','report/VisitMensBetas.eps');
 
 YXVisitNoemailBal = balanceSamples(YXVisitNoemail);
 [b, VisitNoemailBetas, VisitNoemailLCLs] = logisticRegression(YXVisitNoemailBal, epochs, lambda, beta0, 1, [], alpha );
 
-figure (14); plot(VisitNoemailLCLs,'-s'); title("VisitNoemailLCLs"); xlabel("Epoch"); ylabel("LCL");
+figure (14); plot(VisitNoemailLCLs,'-s'); title('VisitNoemailLCLs'); xlabel('Epoch'); ylabel('LCL');
 print('-depsc2','report/VisitNoemailLCLs.eps');
-figure (15); plot(VisitNoemailBetas(:,1),'-','linewidth',5,VisitNoemailBetas(:,2:end),'-'); title("VisitNoemailBetas"); xlabel("Epoch");
+figure (15); plot(VisitNoemailBetas(:,1),'-','linewidth',5,VisitNoemailBetas(:,2:end),'-'); title('VisitNoemailBetas'); xlabel('Epoch');
 print('-depsc2','report/VisitNoemailBetas.eps');
 
 YXPurchaseWomensBal = balanceSamples(YXPurchaseWomens);
 [b, PurchaseWomensBetas, PurchaseWomensLCLs] = logisticRegression(YXPurchaseWomensBal, epochs, lambda, beta0, 1, [], alpha );
 
-figure (16); plot(PurchaseWomensLCLs,'-s'); title("PurchaseWomensLCLs"); xlabel("Epoch"); ylabel("LCL");
+figure (16); plot(PurchaseWomensLCLs,'-s'); title('PurchaseWomensLCLs'); xlabel('Epoch'); ylabel('LCL');
 print('-depsc2','report/PurchaseWomensLCLs.eps');
-figure (17); plot(PurchaseWomensBetas(:,1),'-','linewidth',5,PurchaseWomensBetas(:,2:end),'-'); title("PurchaseWomensBetas"); xlabel("Epoch");
+figure (17); plot(PurchaseWomensBetas(:,1),'-','linewidth',5,PurchaseWomensBetas(:,2:end),'-'); title('PurchaseWomensBetas'); xlabel('Epoch');
 print('-depsc2','report/PurchaseWomensBetas.eps');
 
 YXPurchaseMensBal = balanceSamples(YXPurchaseMens);
 [b, PurchaseMensBetas, PurchaseMensLCLs] = logisticRegression(YXPurchaseMensBal, epochs, lambda, beta0, 1, [], alpha );
 
-figure (18); plot(PurchaseMensLCLs,'-s'); title("PurchaseMensLCLs"); xlabel("Epoch"); ylabel("LCL");
+figure (18); plot(PurchaseMensLCLs,'-s'); title('PurchaseMensLCLs'); xlabel('Epoch'); ylabel('LCL');
 print('-depsc2','report/PurchaseMensLCLs.eps');
-figure (19); plot(PurchaseMensBetas(:,1),'-','linewidth',5,PurchaseMensBetas(:,2:end),'-'); title("PurchaseMensBetas"); xlabel("Epoch");
+figure (19); plot(PurchaseMensBetas(:,1),'-','linewidth',5,PurchaseMensBetas(:,2:end),'-'); title('PurchaseMensBetas'); xlabel('Epoch');
 print('-depsc2','report/PurchaseMensBetas.eps');
 
 YXPurchaseNoemailBal = balanceSamples(YXPurchaseNoemail);
 [b, PurchaseNoemailBetas, PurchaseNoemailLCLs] = logisticRegression(YXPurchaseNoemailBal, epochs, lambda, beta0, 1, [], alpha );
 
-figure (20); plot(PurchaseNoemailLCLs,'-s'); title("PurchaseNoemailLCLs"); xlabel("Epoch"); ylabel("LCL");
+figure (20); plot(PurchaseNoemailLCLs,'-s'); title('PurchaseNoemailLCLs'); xlabel('Epoch'); ylabel('LCL');
 print('-depsc2','report/PurchaseNoemailLCLs.eps');
-figure (21); plot(PurchaseNoemailBetas(:,1),'-','linewidth',5,PurchaseNoemailBetas(:,2:end),'-'); title("PurchaseNoemailBetas"); xlabel("Epoch");
+figure (21); plot(PurchaseNoemailBetas(:,1),'-','linewidth',5,PurchaseNoemailBetas(:,2:end),'-'); title('PurchaseNoemailBetas'); xlabel('Epoch');
 print('-depsc2','report/PurchaseNoemailBetas.eps');
 
 [m, i] = max(VisitWomensLCLs);
