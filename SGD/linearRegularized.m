662c03044d884124416fcc33159fc58247478645
Spencer Bliven <spencer.bliven@gmail.com>
2011-02-08 01:18:15 -0800

    put linearRegularized into homogeneous coords

--- SGD/linearRegularized.m
+++ SGD/linearRegularized.m
@@ -1,9 +1,10 @@
-function [y, dy] = linear(b,x, alpha)
+function [y, dy] = linear(b,yx, alpha)
 % calculates dot product
 %   yi = b1*xi1 + b2*xi2 + ... + bn*xin
 % and gradient
 %   dyi/dbj = xij
-
+x = yx;
+x(:,1) = 1;
 y = x*b-alpha*b'*b;
 
 if nargout > 1
