68dfc1849dcb63830722c372558c4087635f4ca8
Spencer Bliven <spencer.bliven@gmail.com>
2011-02-26 14:59:46 -0800

    Improved matlab compatability

--- CRF/main.m
+++ CRF/main.m
@@ -8,19 +8,19 @@
 %  1. j
 %  2. i
 
-f, numWords, numTags, J, maxI = loadFeatures("FeatureValues/Zulu.win3-7.values.txt");
+[f, numWords, numTags, J, maxI] = loadFeatures('FeatureValues/Zulu.win3-7.values.txt');
 
 %% usefull operations:
 
 %% visualize (J,I) matrix for a particular x and y
-spy(f{2}{1,1})
+spy(f{2}{1,1});
 
 %% Visualize non-zero features over all tags
 n = 2;
 s = sparse(J,maxI);
 for t1 = 1:numTags,
 for t2 = 1:numTags,
-s = s.+ f{n}{t1,t2};
+s = s + f{n}{t1,t2};
 end
 end
 spy(s);
