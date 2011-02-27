404c84f2f2c0917bee014a40c34854daf9f7748b
Spencer Bliven <spencer.bliven@gmail.com>
2011-02-26 19:22:55 -0800

    Added -1 for unused labels

--- CRF/FeatureGenerating/learnSyllables.py
+++ CRF/FeatureGenerating/learnSyllables.py
@@ -10,7 +10,7 @@ from syllableParser import parseSyllableFile
 from WindowFeature import WindowFeature
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser( usage="usage: python %prog [options] inputFile valueFile featureFile wordFile" )
+    parser = optparse.OptionParser( usage="usage: python %prog [options] inputFile valueFile featureFile wordFile labelFile" )
     parser.add_option("-k","--min-k", help="minimum window length",
         dest="minK",default=3,type="int")
     parser.add_option("-l","--max-k", help="maximum window length",
@@ -19,20 +19,27 @@ if __name__ == "__main__":
     parser.add_option("-a","--ambiguous", help="Include ambiguous words (2 possible syllable patterns)", dest="includeAmbiguous",default=False, action="store_true")
     (options, args) = parser.parse_args()
 
-    if len(args) != 4:
+    if len(args) != 5:
         parser.print_usage()
-        parser.exit("Error: Expected 4 argument, but found %d"%len(args) )
+        parser.exit("Error: Expected 5 argument, but found %d"%len(args) )
 
 
     inputFilename = args[0]
     valueFilename = args[1]
     nameFilename = args[2]
     wordFilename = args[3]
+    labelFilename = args[4]
 
     trainData = list(parseSyllableFile(inputFilename,options.useLineNums, options.includeAmbiguous))
     with open(wordFilename,"w") as wordFile:
         for word, label, index in trainData:
             wordFile.write("%d\t%s\t%s\n" % (index,word,label))
+    with open(labelFilename,"w") as labelFile:
+        maxI = max( [len(l) for w,l,i in trainData] )
+        for word, label, index in trainData:
+            labelFile.write( " ".join( [tag for tag in label] + ["-1"]*(maxI-len(label)+1) ))
+            labelFile.write("\n")
+
 
     with open(nameFilename,"w") as nameFile:
 
